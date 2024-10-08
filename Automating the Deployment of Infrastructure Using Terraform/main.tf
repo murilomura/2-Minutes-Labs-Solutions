78054345c4972e7f9ea774374e121a0120b68e86
murilomura <63733809+murilomura@users.noreply.github.com>
2024-10-08 19:35:06 -0300

    Update main.tf

--- Automating the Deployment of Infrastructure Using Terraform/main.tf
+++ Automating the Deployment of Infrastructure Using Terraform/main.tf
@@ -11,16 +11,13 @@ resource "google_compute_instance" "vm_instance" {
   name         = "${var.instance_name}"
   zone         = "${var.instance_zone}"
   machine_type = "${var.instance_type}"
-
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-    }
+      }
   }
-
   network_interface {
-    subnetwork = "${var.instance_subnetwork}"
-
+    network = "${var.instance_network}"
     access_config {
       # Allocate a one-to-one NAT IP to the instance
     }
